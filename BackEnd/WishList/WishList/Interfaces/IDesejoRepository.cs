417e11f88ad1827cee90887f03ec7dae374f427f
Leandro Cavalcanti <73174539+LeandroMCF@users.noreply.github.com>
2021-05-26 18:28:23 -0300

    iniciando bked

---
+++ BackEnd/WishList/WishList/Interfaces/IDesejoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WishList.Domains;
+
+namespace WishList.Interfaces
+{
+    interface IDesejoRepository
+    {
+        List<Desejo> Listar();
+        void Cadastrar(Desejo novoDesejo);
+        void Deletar(int id);
+        List<Desejo> ListarPorId(int id);
+    }
+}
